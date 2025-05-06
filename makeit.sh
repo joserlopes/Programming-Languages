204cfb8aed9ebd27393b16904e7eb0a7728e1043
José Lopes <josersl2003@gmail.com>
2025-05-06 18:05:17 +0100

    chore: Change project to work with maven.

--- makeit.sh
+++ makeit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-rm ./src/*.class
-javacc -OUTPUT_DIRECTORY=./src ./src/ParserL0.jj
-javac ./src/*.java
+mvn compile
+# mvn exec:java
+
+# rm ./src/main/*.class
+# javacc -OUTPUT_DIRECTORY=./src/generated ./src/main/ParserL0.jj
+# javac ./src/main/*.java
