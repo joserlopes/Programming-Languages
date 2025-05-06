43880d75020909c4bd73a50f62715109df48187b
José Lopes <josersl2003@gmail.com>
2025-05-06 11:49:18 +0100

    Initial Structure of the Project.

---
+++ ASTInt.java
@@ -0,0 +1,14 @@
+class ASTInt implements ASTNode  {
+    int v;
+
+    ASTInt(int v0) {
+        v = v0;
+    }
+
+    public IValue eval(Environment<IValue> e) throws InterpreterError
+    {
+	e.crawl();
+	return new VInt(v);                
+    }
+
+}
