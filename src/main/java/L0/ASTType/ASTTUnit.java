de7f63273ecfcbf55a0ee1684229c89301091500
José Lopes <josersl2003@gmail.com>
2025-06-12 15:23:27 +0100

    fix(P2): Consider unit to be a subtype of all types.

--- src/main/java/L0/ASTType/ASTTUnit.java
+++ src/main/java/L0/ASTType/ASTTUnit.java
@@ -1,7 +1,14 @@
 package L0.ASTType;
 
+import L0.Environment;
+import L0.Errors.InterpreterError;
+
 public class ASTTUnit implements ASTType {
   public ASTTUnit() {}
+
+  public boolean isSubtype(ASTType other, Environment<ASTType> e) throws InterpreterError {
+    return true;
+  }
 
   public String toStr() {
     return "()";
